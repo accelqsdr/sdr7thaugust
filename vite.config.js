9a8bff5b4b494617ba508587e16b026b7a2305f8
accelqsdr <rajesh.botta@accelq.com>
2026-09-03 22:15:45 +0530

    Trigger Netlify rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
+// build: 1788453944925
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash]-b2.js`,
-        chunkFileNames: `assets/[name]-[hash]-b2.js`,
-        assetFileNames: `assets/[name]-[hash].[ext]`,
-      }
-    }
-  }
 })
